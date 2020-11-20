ab68b75b487b3b73d43dd23d1a8a0e200eefb67e
Alexey Pyrta <pyrta90@gmail.com>
2020-11-20 04:13:48 +0400

    add other methods

--- function.php
+++ function.php
@@ -13,6 +13,48 @@ function getPosts($connect) {
 
 function getPost($connect, $id) {
     $post = mysqli_query($connect, "SELECT * FROM `posts` WHERE `id` = '$id'");
-    $post = mysqli_fetch_assoc($post);
-    echo json_encode($post);
+
+    if (mysqli_num_rows($post) === 0) {
+        http_response_code(404);
+        $res = [
+            "status" => false,
+            "message" => "post not found"
+        ];
+        echo json_encode($res);
+    } else {
+        $post = mysqli_fetch_assoc($post);
+        echo json_encode($post);
+    }
+}
+
+function addPost($connect, $data) {
+    $title = $data['title'];
+    $body = $data['body'];
+    http_response_code(201);
+    mysqli_query($connect, "INSERT INTO `posts` (`id`, `title`, `body`) VALUES (NULL, '$title', '$body')");
+    $res = [
+        "status" => true,
+        "post_id" => mysqli_insert_id($connect)
+    ];
+    echo json_encode($res);
+}
+function updatePost($connect, $data, $id) {
+    http_response_code(200);
+    $title = $data['title'];
+    $body = $data['body'];
+    mysqli_query($connect, "UPDATE `posts` SET `title` = '$title', `body` = '$body' WHERE `posts`.`id` = '$id'");
+    $res = [
+        "status" => true,
+        "message" => "post updated"
+    ];
+    echo json_encode($res);
+}
+function deletePost($connect, $id) {
+    
+    mysqli_query($connect, "DELETE FROM `posts` WHERE `posts`.`id` = '$id'");
+    $res = [
+        "status" => true,
+        "message" => "post deleted"
+    ];
+    echo json_encode($res);
 }
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Credentials: true');
 header('Content-Type: application/json');
 require "connect.php";
 require "function.php";
@@ -10,12 +13,37 @@ $params = explode('/', $q);
 $type = $params[0];
 $id = $params[1];
 
+$method = $_SERVER['REQUEST_METHOD'];
 
-if ($type === 'posts') {
-    if (isset($id)) {
-        getPost($connect, $id);
-    }else {
-        getPosts($connect);
-    }   
+switch ($method) {
+    case 'GET':
+        if ($type === 'posts') {
+            if (isset($id)) {
+                getPost($connect, $id);
+            }else {
+                getPosts($connect);
+            }   
+        }
+        break;
+    case 'POST':
+        addPost($connect, $_POST);
+        break;
+    case 'PATCH':
+        if ($type === 'posts') {
+            if (isset($id)) {
+                $data = file_get_contents('php://input');
+                $data = json_decode($data, true);
+                updatePost($connect, $data, $id);
+            }  
+        }
+        break;  
+    case 'DELETE':
+        if ($type === 'posts') {
+            if (isset($id)) {
+                deletePost($connect, $id);
+            }  
+        }
+        break;
 }
+
 
